fbf3c63fc43566075da57f07f5211e371a1c1b7a
Evgeny Danilenko <eugene.danilenko@lazada.com>
2017-05-11 19:07:03 +0300

    More include selected fields tests

--- tests/include_test.go
+++ tests/include_test.go
@@ -19,3 +19,45 @@ func TestIncludeFields(t *testing.T) {
 		t.Fatal("Got", string(res), "\t Expected", expected)
 	}
 }
+
+func TestIncludeFields_FirstField(t *testing.T) {
+	n := ExcludeFields{1, 2, []string{"Int1"}}
+
+	res, err := easyjson.Marshal(n)
+	if err != nil {
+		t.Fatal("Got an error", err)
+	}
+
+	expected := `{"Int1":1}`
+	if !bytes.Equal(res, []byte(expected)) {
+		t.Fatal("Got", string(res), "\t Expected", expected)
+	}
+}
+
+func TestIncludeFields_All_EmptyIncludeFields(t *testing.T) {
+	n := ExcludeFields{1, 2, []string{}}
+
+	res, err := easyjson.Marshal(n)
+	if err != nil {
+		t.Fatal("Got an error", err)
+	}
+
+	expected := `{"Int1":1,"Int2":2}`
+	if !bytes.Equal(res, []byte(expected)) {
+		t.Fatal("Got", string(res), "\t Expected", expected)
+	}
+}
+
+func TestIncludeFields_All_AllFields(t *testing.T) {
+	n := ExcludeFields{1, 2, []string{"Int1", "Int2"}}
+
+	res, err := easyjson.Marshal(n)
+	if err != nil {
+		t.Fatal("Got an error", err)
+	}
+
+	expected := `{"Int1":1,"Int2":2}`
+	if !bytes.Equal(res, []byte(expected)) {
+		t.Fatal("Got", string(res), "\t Expected", expected)
+	}
+}
